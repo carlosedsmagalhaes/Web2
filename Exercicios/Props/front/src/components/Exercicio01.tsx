9094a80cd4f8c289a2efaf5241fe5609d5ad464b
Carlos Magalhães <carlosedsmagalhaes@gmail.com>
2025-03-19 00:55:19 -0300

    Remove 1 do número sorteado no componente FormRandom para limitar o intervalo a 0-99

--- Exercicios/Props/front/src/components/Exercicio01.tsx
+++ Exercicios/Props/front/src/components/Exercicio01.tsx
@@ -13,7 +13,7 @@ export default function FormRandom(props: Props) {
     let numerosSorteados: number[] = [];
     let limite: number = qtd > 12 ? 12 : qtd;
     for (let index = 0; index < limite; index++) {
-      const num: number = Math.floor(Math.random() * 100 + 1);
+      const num: number = Math.floor(Math.random() * 100);
       numerosSorteados.push(num);
 
       numerosSorteados.sort((a, b) => a - b);
